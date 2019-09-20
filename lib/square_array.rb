a8cc895842e6d194ae58ce403720164a208e6ec4
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-09-20 20:57:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,8 +3,9 @@ def square_array(array)
   counter = 0  
   
   while counter < array.length do
-    array[counter] = array[counter]**2
+    array[counter]**2
     counter +=1
+    return array[counter]
   end
 end
 
