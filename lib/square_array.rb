94bab393cd02a581b0e78e658be4bf810afb0c25
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-09-20 20:44:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
   # your code here
+  counter = 0  
+  
+  while counter <= array.length do
+    array[counter]**2
+    counter +=1
 end
+end
+
+#numbers = [1,2,3]
+#square_array(numbers)
+
+#puts numbers
+
